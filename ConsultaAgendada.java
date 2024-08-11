4ae38a6aedec178510924015051b68b5ad71be2b
JMsPoP <121116067+JMsPoP@users.noreply.github.com>
2024-08-11 12:55:07 -0300

    prova 2

--- ConsultaAgendada.java
+++ ConsultaAgendada.java
@@ -1,30 +1,3 @@
-/*
-* Reescreva as propriedades e métodos da classe Data, deixando-os de acordo com
-* o padrão UML (Getter e Setter).
-* @author: João Marcos Teles Silva CB3026787
-* O construtor ConsultaAgendada deve nos permitir a digitação dos valores de data, hora, nome do paciente e do médico;
-* Os outros dois construtores devem receber os valores de data, hora, nome do
-* paciente e do médico sob a forma de parâmetros;
-* Qualquer construtor chamado deve acrescer 1 no atributo quantidade.
-* Os métodos setData(), setHora(), setNomePaciente(), setNomeMedico() devem nos
-* permitir alterar os valores das respectivas propriedades através da digitação de
-* novos valores;
-* Os demais métodos “set” devem alterar os valores das propriedades a partir dos
-* parâmetros recebidos;
-* O método getData() deve nos devolver a data no formato: dd/mm/aa;
-* O método getHora() deve nos devolver a hora no formato: hh:mm:ss.
-* Usando a classe ConsultaAgendada instancie o objeto p1 inicializado-o com o
-* construtor ConsultaAgendada (int h, int mi, int s, int d, int m, int a, String p, String m);
-* Exiba todas as propriedades de p1;
-* Agora instancie o objeto p2 usando o construtor ConsultaAgendada ();
-* Exiba todas as propriedades de p2;
-* Usando os métodos setData(), setHora(), setNomePaciente(), setNomeMedico altere
-* as propriedades de p1;
-* Exiba todas as propriedades de p1 novamente.
-* Exiba a quantidade final de consultas.
-*/
-
-
 import java.util.Scanner;
 
 public class ConsultaAgendada {
@@ -53,9 +26,7 @@ public class ConsultaAgendada {
     }
 
     public ConsultaAgendada(int h, int mi, int s, int d, int m, int a, String p, String nomeMedico) {
-        // Inicialização do objeto 'hora'
         this.hora = new Hora(); 
-
         setHora(h, mi, s);
         setData(d, m, a);
         setNomePaciente(p);
